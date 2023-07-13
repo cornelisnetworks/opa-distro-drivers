3060cf0d3521e64f10c8e639edc0327702a94201
Brendan Cunningham <bcunningham@cornelisnetworks.com>
2023-07-13 10:01:23 -0400

    INTERNAL_GPU: Add context get, put callbacks to sdma_txadd_daddr()
    ctx_get, ctx_put callbacks are needed for adding SDMA descriptors for
    non-system-memory where that memory is managed in a pinning cache, e.g.
    GPU memory.
    
    INTERNAL: STL-65795 Merge NVIDIA pinning changes into c-stream-master
    
    Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
    (cherry picked from commit e2a809bc8cd85d1b5a82e47b2bcf63f96688b0df)
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
    (cherry picked from commit 57a9453c1a8ac5f567f2b9e7bc2ecd7653801d9e)
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/verbs.c
+++ drivers/infiniband/hw/hfi1/verbs.c
@@ -779,7 +779,7 @@ static int build_verbs_tx_desc(
 	/* add icrc, lt byte, and padding to flit */
 	if (extra_bytes)
 		ret = sdma_txadd_daddr(sde->dd, &tx->txreq, sde->dd->sdma_pad_phys,
-				       extra_bytes);
+				       extra_bytes, NULL, NULL, NULL);
 
 bail_txadd:
 	return ret;
