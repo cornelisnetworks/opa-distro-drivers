cornelisnetworks/opa-distro-drivers

commit 435467fd02030be73ed91bae3b71e0ddf7506835

IB/hfi1: Place struct mmu_rb_handler on cache line start
Place struct mmu_rb_handler on cache line start like so:

	struct mmu_rb_handler *h;
	void *free_ptr;
	int ret;

	free_ptr = kzalloc(sizeof(*h) + cache_line_size() - 1, GFP_KERNEL);
	if (!free_ptr)
		return -ENOMEM;

	h = PTR_ALIGN(free_ptr, cache_line_size());

Additionally, move struct mmu_rb_handler fields "root" and "ops_args" to
start after the next cacheline using the "____cacheline_aligned_in_smp"
annotation.

Allocating an additional cache_line_size() - 1 bytes to place
struct mmu_rb_handler on a cache line start does increase memory
consumption.

However, few struct mmu_rb_handler are created when hfi1 is in use.
As mmu_rb_handler->root and mmu_rb_handler->ops_args are accessed
frequently, the advantage of having them both within a cache line is
expected to outweigh the disadvantage of the additional memory
consumption per struct mmu_rb_handler.

Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
Signed-off-by: Patrick Kelsey <pat.kelsey@cornelisnetworks.com>
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
Link: https://lore.kernel.org/r/168088636963.3027109.16959757980497822530.stgit@252.162.96.66.static.eigbox.net
Signed-off-by: Leon Romanovsky <leon@kernel.org>
(cherry picked from commit 579169aef6f710f0dae0d50d05559b9aa0b96348)

---
The mmu_notifer in RHEL 8.x does not have a pointer to the mm. Instead RedHat
has a special sauce struct they tack on the end of it. After decoding the macro
hell it's mn._rh->mm

drivers/infiniband/hw/hfi1/mmu_rb.c
@@ -48,12 +48,14 @@ int hfi1_mmu_rb_register(void *ops_arg,
 			 struct mmu_rb_handler **handler)
 {
 	struct mmu_rb_handler *h;
+	void *free_ptr;
 	int ret;
 
-	h = kzalloc(sizeof(*h), GFP_KERNEL);
-	if (!h)
+	free_ptr = kzalloc(sizeof(*h) + cache_line_size() - 1, GFP_KERNEL);
+	if (!free_ptr)
 		return -ENOMEM;
 
+	h = PTR_ALIGN(free_ptr, cache_line_size());
 	h->root = RB_ROOT_CACHED;
 	h->ops = ops;
 	h->ops_arg = ops_arg;
@@ -64,14 +66,15 @@ int hfi1_mmu_rb_register(void *ops_arg,
 	INIT_LIST_HEAD(&h->del_list);
 	INIT_LIST_HEAD(&h->lru_list);
 	h->wq = wq;
+	h->free_ptr = free_ptr;
 
 	ret = mmu_notifier_register(&h->mn, current->mm);
 	if (ret) {
-		kfree(h);
+		kfree(free_ptr);
 		return ret;
 	}
 
-	h->mm = current->mm;
+	h->mn._rh->mm = current->mm;
 	*handler = h;
 	return 0;
 }
@@ -84,10 +87,10 @@ void hfi1_mmu_rb_unregister(struct mmu_rb_handler *handler)
 	struct list_head del_list;
 
 	/* Prevent freeing of mm until we are completely finished. */
-	mmgrab(handler->mm);
+	mmgrab(handler->mn._rh->mm);
 
 	/* Unregister first so we don't get any more notifications. */
-	mmu_notifier_unregister(&handler->mn, handler->mm);
+	mmu_notifier_unregister(&handler->mn, handler->mn._rh->mm);
 
 	/*
 	 * Make sure the wq delete handler is finished running.  It will not
@@ -109,9 +112,9 @@ void hfi1_mmu_rb_unregister(struct mmu_rb_handler *handler)
 	do_remove(handler, &del_list);
 
 	/* Now the mm may be freed. */
-	mmdrop(handler->mm);
+	mmdrop(handler->mn._rh->mm);
 
-	kfree(handler);
+	kfree(handler->free_ptr);
 }
 
 int hfi1_mmu_rb_insert(struct mmu_rb_handler *handler,
@@ -123,7 +126,7 @@ int hfi1_mmu_rb_insert(struct mmu_rb_handler *handler,
 
 	trace_hfi1_mmu_rb_insert(mnode->addr, mnode->len);
 
-	if (current->mm != handler->mm)
+	if (current->mm != handler->mn._rh->mm)
 		return -EPERM;
 
 	spin_lock_irqsave(&handler->lock, flags);
@@ -190,7 +193,7 @@ void hfi1_mmu_rb_evict(struct mmu_rb_handler *handler, void *evict_arg)
 	unsigned long flags;
 	bool stop = false;
 
-	if (current->mm != handler->mm)
+	if (current->mm != handler->mn._rh->mm)
 		return;
 
 	INIT_LIST_HEAD(&del_list);

drivers/infiniband/hw/hfi1/mmu_rb.h
@@ -33,16 +33,25 @@ struct mmu_rb_ops {
 };
 
 struct mmu_rb_handler {
+	/*
+	 * struct mmu_notifier is 56 bytes, and spinlock_t is 4 bytes, so
+	 * they fit together in one cache line.  mn is relatively rarely
+	 * accessed, so co-locating the spinlock with it achieves much of
+	 * the cacheline contention reduction of giving the spinlock its own
+	 * cacheline without the overhead of doing so.
+	 */
 	struct mmu_notifier mn;
-	struct mm_struct *mm;
-	struct rb_root_cached root;
-	void *ops_arg;
 	spinlock_t lock;        /* protect the RB tree */
+
+	/* Begin on a new cachline boundary here */
+	struct rb_root_cached root ____cacheline_aligned_in_smp;
+	void *ops_arg;
 	struct mmu_rb_ops *ops;
 	struct list_head lru_list;
 	struct work_struct del_work;
 	struct list_head del_list;
 	struct workqueue_struct *wq;
+	void *free_ptr;
 };
 
 int hfi1_mmu_rb_register(void *ops_arg,

drivers/infiniband/hw/hfi1/user_sdma.h
@@ -207,7 +207,7 @@ int hfi1_user_sdma_process_request(struct hfi1_filedata *fd,
 
 static inline struct mm_struct *mm_from_sdma_node(struct sdma_mmu_node *node)
 {
-	return node->rb.handler->mm;
+	return node->rb.handler->mn._rh->mm;
 }
 
 #endif /* _HFI1_USER_SDMA_H */

commit 73f085dc689e644f31b003369e3801671b7757ce

RDMA/hfi1: fix use-after-free in hfi1_mmu_rb_unregister()
(*struct mmu_notifier._rh) is kfree()'d in mmu_notifier_unregister().
However, hfi1_mmu_rb_unregister() uses struct mmu_notifier._rh after
calling mmu_notifier_unregister(). This use-after-free can cause a
kernel panic like so:

    [280321.095739] BUG: unable to handle kernel NULL pointer dereference at 000000000000005c
    [280321.104909] PGD 80000006cfdd2067 P4D 80000006cfdd2067 PUD 112330067 PMD 0
    [280321.112853] Oops: 0002 [#1] SMP PTI
    [280321.117054] CPU: 52 PID: 1449534 Comm: osu_alltoallv Kdump: loaded Tainted: G           OE    --------- -  - 4.18.0-425.3.1.el8.x86_64 #1
    [280321.131359] Hardware name: Intel Corporation S2600KP/S2600KP, BIOS SE5C610.86B.01.01.3029.C4.022420221031 02/24/2022
    [280321.143417] RIP: 0010:hfi1_mmu_rb_unregister+0x15d/0x1a0 [hfi1]
    [280321.150360] Code: 8d 7b 10 48 89 6b 08 f0 ff 4b 10 0f 88 d0 8c 05 00 75 c0 e8 15 f7 ff ff 48 8b 44 24 10 49 39 c5 75 bb 49 8b 44 24 20 48 8b 38 <f0> ff 4f 5c 74 30 49 8b bc 24 e8 00 00 00 e8 b0 d8 35 f4 48 8b 44
    [280321.171773] RSP: 0018:ffff9bad47863d98 EFLAGS: 00010246
    [280321.177876] RAX: ffff8dc556b1c3c0 RBX: 0000000000000000 RCX: 0000000000000000
    [280321.186180] RDX: 0000000000000001 RSI: 0000000000000282 RDI: 0000000000000000
    [280321.194420] RBP: dead000000000200 R08: 00000000001e66ae R09: 00000000001e66ae
    [280321.202658] R10: 0000000000000000 R11: ffff8dc59fc29c04 R12: ffff8dc1f25eb400
    [280321.210915] R13: ffff9bad47863da8 R14: dead000000000100 R15: ffff8dc12575e000
    [280321.219216] FS:  00007fd3ce697740(0000) GS:ffff8dc59fc00000(0000) knlGS:0000000000000000
    [280321.228523] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
    [280321.235258] CR2: 000000000000005c CR3: 000000054080c004 CR4: 00000000003706e0
    [280321.243500] DR0: 0000000000000000 DR1: 0000000000000000 DR2: 0000000000000000
    [280321.251754] DR3: 0000000000000000 DR6: 00000000fffe0ff0 DR7: 0000000000000400
    [280321.260045] Call Trace:
    [280321.263108]  free_pinning_interfaces+0x2a/0x40 [hfi1]
    [280321.269119]  hfi1_user_sdma_free_queues+0x99/0x220 [hfi1]
    [280321.275453]  hfi1_file_close+0x6d/0x1b0 [hfi1]
    [280321.280708]  __fput+0xbe/0x250
    [280321.284404]  task_work_run+0x8a/0xb0
    [280321.288666]  exit_to_usermode_loop+0xef/0x100
    [280321.293801]  do_syscall_64+0x19c/0x1b0
    [280321.298288]  entry_SYSCALL_64_after_hwframe+0x61/0xc6
    [280321.304249] RIP: 0033:0x7fd3cdb9cb47
    [280321.308496] Code: 12 b8 03 00 00 00 0f 05 48 3d 00 f0 ff ff 77 3b c3 66 90 53 89 fb 48 83 ec 10 e8 e4 fb ff ff 89 df 89 c2 b8 03 00 00 00 0f 05 <48> 3d 00 f0 ff ff 77 2b 89 d7 89 44 24 0c e8 26 fc ff ff 8b 44 24
    [280321.329946] RSP: 002b:00007ffcbe3f4160 EFLAGS: 00000293 ORIG_RAX: 0000000000000003
    [280321.338681] RAX: 0000000000000000 RBX: 0000000000000012 RCX: 00007fd3cdb9cb47
    [280321.346962] RDX: 0000000000000000 RSI: 0000000000001000 RDI: 0000000000000012
    [280321.355269] RBP: 0000000000000000 R08: 0000000000000000 R09: 00007ffcbe3f4170
    [280321.363524] R10: 00007fd3bbfc2e20 R11: 0000000000000293 R12: 00000000017acc80
    [280321.371777] R13: 0000000000000001 R14: 0000000077359400 R15: 00000000017acc80
    [280321.380093] Modules linked in: ib_ipoib hfi1(OE) rdmavt(OE) rpcrdma rdma_ucm ib_srpt ib_isert iscsi_target_mod target_core_mod ib_iser libiscsi scsi_transport_iscsi opa_vnic ib_umad rdma_cm iw_cm ib_cm sunrpc intel_rapl_msr intel_rapl_common sb_edac x86_pkg_temp_thermal intel_powerclamp coretemp kvm_intel kvm irqbypass crct10dif_pclmul crc32_pclmul ghash_clmulni_intel rapl iTCO_wdt iTCO_vendor_support intel_cstate mxm_wmi ipmi_si intel_uncore ipmi_devintf ioatdma mei_me pcspkr ftdi_sio joydev mei ipmi_msghandler i2c_i801 lpc_ich wmi acpi_power_meter acpi_pad xfs libcrc32c sd_mod t10_pi sg mgag200 drm_shmem_helper drm_kms_helper syscopyarea sysfillrect sysimgblt fb_sys_fops ib_uverbs ahci igb libahci drm crc32c_intel ib_core libata dca i2c_algo_bit fuse [last unloaded: hfi1]
    [280321.458486] CR2: 000000000000005c

pin_system.c free_system_node() is also guilty of use-after-free of
struct mmu_notifier._rh.

Fix this by adding the field 'struct mm_struct *mm' to struct
mmu_rb_handler. Initialize struct mmu_rb_handler.mm after mmu_rb calls
mmu_notifier_register(). Use struct mmu_rb_handler.mm instead of struct
mmu_rb_handler.mn->_rh.mm.

Fixes: 435467fd0203 ("IB/hfi1: Place struct mmu_rb_handler on cache line start")

drivers/infiniband/hw/hfi1/mmu_rb.c
@@ -73,7 +73,7 @@ static int _hfi1_mmu_rb_register(void *ops_arg,
 		return ret;
 	}
 
-	h->mn._rh->mm = current->mm;
+	h->mm = current->mm;
 	*handler = h;
 	return 0;
 }
@@ -94,10 +94,10 @@ void hfi1_mmu_rb_unregister(struct mmu_rb_handler *handler)
 	struct list_head del_list;
 
 	/* Prevent freeing of mm until we are completely finished. */
-	mmgrab(handler->mn._rh->mm);
+	mmgrab(handler->mm);
 
 	/* Unregister first so we don't get any more notifications. */
-	mmu_notifier_unregister(&handler->mn, handler->mn._rh->mm);
+	mmu_notifier_unregister(&handler->mn, handler->mm);
 
 	/*
 	 * Make sure the wq delete handler is finished running.  It will not
@@ -123,7 +123,7 @@ void hfi1_mmu_rb_unregister(struct mmu_rb_handler *handler)
 	}
 
 	/* Now the mm may be freed. */
-	mmdrop(handler->mn._rh->mm);
+	mmdrop(handler->mm);
 
 	kfree(handler->free_ptr);
 }
@@ -137,7 +137,7 @@ int hfi1_mmu_rb_insert(struct mmu_rb_handler *handler,
 
 	trace_hfi1_mmu_rb_insert(mnode);
 
-	if (current->mm != handler->mn._rh->mm)
+	if (current->mm != handler->mm)
 		return -EPERM;
 
 	spin_lock_irqsave(&handler->lock, flags);
@@ -241,7 +241,7 @@ void hfi1_mmu_rb_evict(struct mmu_rb_handler *handler, void *evict_arg)
 	unsigned long flags;
 	bool stop = false;
 
-	if (current->mm != handler->mn._rh->mm)
+	if (current->mm != handler->mm)
 		return;
 
 	INIT_LIST_HEAD(&del_list);
@@ -464,11 +464,7 @@ void hfi1_gpu_cache_invalidate(struct mmu_rb_handler *handler,
 			       unsigned long start, unsigned long end)
 {
 	struct mmu_notifier_range r = {
-#ifdef NO_MMU_NOTIFIER_MM
 		.mm = handler->mm,
-#else
-		.mm = handler->mn._rh->mm,
-#endif
 		.start = start,
 		.end = end,
 	};

drivers/infiniband/hw/hfi1/mmu_rb.h
@@ -39,13 +39,15 @@ struct mmu_rb_ops {
 
 struct mmu_rb_handler {
 	/*
-	 * struct mmu_notifier is 56 bytes, and spinlock_t is 4 bytes, so
-	 * they fit together in one cache line.  mn is relatively rarely
-	 * accessed, so co-locating the spinlock with it achieves much of
-	 * the cacheline contention reduction of giving the spinlock its own
-	 * cacheline without the overhead of doing so.
+	 * struct mmu_notifier is 40 bytes, struct mm_struct* is 8 bytes, and
+	 * spinlock_t is 4 bytes, so they fit together in one cache line.
+	 *
+	 * mn is relatively rarely accessed, so co-locating the spinlock with
+	 * it achieves much of the cacheline contention reduction of giving the
+	 * spinlock its own cacheline without the overhead of doing so.
 	 */
 	struct mmu_notifier mn;
+	struct mm_struct *mm;
 	spinlock_t lock;        /* protect the RB tree */
 
 	/* Begin on a new cachline boundary here */

drivers/infiniband/hw/hfi1/pin_system.c
@@ -83,7 +83,7 @@ static void unpin_vector_pages(struct mm_struct *mm, struct page **pages,
 
 static inline struct mm_struct *mm_from_sdma_node(struct sdma_mmu_node *node)
 {
-	return node->rb.handler->mn._rh->mm;
+	return node->rb.handler->mm;
 }
 
 static void free_system_node(struct sdma_mmu_node *node)