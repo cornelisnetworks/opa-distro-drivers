cornelisnetworks/opa-distro-drivers

commit 5189b5ba952ab05139aa0d5fba703c28d82fd47d

INTERNAL_GPU: Add HFI1_IOCTL_TID_UPDATE_V2 for PSM2-CUDA
INTERNAL: STL-67176 Make PSM2-CUDA compatible with new hfi1-NVIDIA
INTERNAL: driver

Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
(cherry picked from commit 8d769a613384cd271a0bd00dd1f698d2f7759a61)
(cherry picked from commit 0aed7424999359481aa6d2f5da614f12f8b37b53)
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
(cherry picked from commit 6cc0d91b10c4fb7be23ee0b37248d7e9a609332c)
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

drivers/infiniband/hw/hfi1/tid_nvidia.c
@@ -56,6 +56,7 @@
 #include "device.h"
 #include "user_exp_rcv.h"
 #include "pinning.h"
+#include "trace_gpu.h"
 
 #define NV_GPU_PAGE_SHIFT 16
 #define NV_GPU_PAGE_SIZE BIT(NV_GPU_PAGE_SHIFT)
@@ -89,6 +90,9 @@ struct nvidia_tid_user_buf {
 	struct list_head nodes;
 	/* Protects .nodes and .invalidated */
 	spinlock_t nodes_lock;
+
+	unsigned long orig_vaddr;
+	unsigned long orig_length;
 };
 
 /*
@@ -251,23 +255,38 @@ static int nvidia_user_buf_init(u16 expected_count,
 				bool notify,
 				unsigned long vaddr,
 				unsigned long length,
+				bool allow_unaligned,
 				struct tid_user_buf **tbuf)
 {
 	struct nvidia_tid_user_buf *nvbuf;
+	unsigned long unaligned_vaddr = vaddr;
 	int ret;
 
-	/* nv-p2p.h nvidia_p2p_get_pages() says vaddr, length must be 64KiB aligned, multiple */
-	if (vaddr % NV_GPU_PAGE_SIZE)
-		return -EINVAL;
-	if (length % NV_GPU_PAGE_SIZE)
-		return -EINVAL;
+	/* Allow non-page-size multiple (vaddr,length) for older userspace. */
+	if (!allow_unaligned) {
+		if (vaddr % NV_GPU_PAGE_SIZE)
+			return -EINVAL;
+		if (length % NV_GPU_PAGE_SIZE)
+			return -EINVAL;
+	}
 
 	nvbuf = kzalloc(sizeof(*nvbuf), GFP_KERNEL);
 	if (!nvbuf)
 		return -ENOMEM;
 	kref_init(&nvbuf->ref);
 	*tbuf = &nvbuf->common;
-	/* Cannot check vaddr alignment here; store for now. */
+
+	/*
+	 * In case vaddr is not aligned to a page boundary,
+	 * round vaddr off to a page boundary and increase
+	 * length to cover the difference.
+	 *
+	 * Store original vaddr, length on nvbuf for tracing.
+	 */
+	nvbuf->orig_vaddr = vaddr;
+	nvbuf->orig_length = length;
+	vaddr = (vaddr & NV_GPU_PAGE_MASK);
+	length += (unaligned_vaddr - vaddr);
 	nvbuf->common.vaddr = vaddr;
 	nvbuf->common.length = length;
 	nvbuf->common.use_mn = notify;
@@ -371,6 +390,8 @@ static int nvidia_pin_pages(struct hfi1_filedata *fd,
 	ret = rdma_interface.get_pages(0, 0, nvbuf->common.vaddr,
 				       nvbuf->common.length, &nvbuf->pages,
 				       nvidia_invalidate_cb, nvbuf);
+	if (ret)
+		trace_recv_pin_gpu_pages_fail(ret, nvbuf->common.vaddr, nvbuf->common.length);
 	/* Don't WARN on these cases */
 	if (ret == -EINVAL || ret == -ENOMEM)
 		return ret;
@@ -380,6 +401,9 @@ static int nvidia_pin_pages(struct hfi1_filedata *fd,
 		ret = -EIO;
 		goto fail_put_pages;
 	}
+
+	trace_pin_rcv_pages_gpu(nvbuf->orig_vaddr, nvbuf->common.vaddr, nvbuf->orig_length,
+				nvbuf->common.length, nvbuf->pages->entries, tbuf);
 
 	if (nvbuf->pages->entries > fd->uctxt->expected_count) {
 		ret = -EINVAL;
@@ -402,6 +426,7 @@ fail_unmap:
 fail_put_pages:
 	WARN_ON(rdma_interface.put_pages(0, 0, nvbuf->common.vaddr, nvbuf->pages));
 	nvbuf->pages = NULL;
+	trace_recv_pin_gpu_pages_fail(ret, nvbuf->common.vaddr, nvbuf->common.length);
 	return ret;
 }
 

do-update-makerpm.sh
@@ -102,6 +102,7 @@ files_to_copy[$modules_cnt]="
 	drivers/infiniband/hw/hfi1/trace.h
 	drivers/infiniband/hw/hfi1/trace_ctxts.h
 	drivers/infiniband/hw/hfi1/trace_dbg.h
+	drivers/infiniband/hw/hfi1/trace_gpu.h
 	drivers/infiniband/hw/hfi1/trace_ibhdrs.h
 	drivers/infiniband/hw/hfi1/trace_misc.h
 	drivers/infiniband/hw/hfi1/trace_rc.h

drivers/infiniband/hw/hfi1/file_ops.c
@@ -49,6 +49,10 @@ static int get_ctxt_info(struct hfi1_filedata *fd, unsigned long arg, u32 len);
 static int get_base_info(struct hfi1_filedata *fd, unsigned long arg, u32 len);
 static int user_exp_rcv_setup(struct hfi1_filedata *fd, unsigned long arg,
 			      u32 len);
+#ifdef NVIDIA_GPU_DIRECT
+static int user_exp_rcv_setup_v2(struct hfi1_filedata *fd, unsigned long arg,
+				 u32 len);
+#endif
 static int user_exp_rcv_clear(struct hfi1_filedata *fd, unsigned long arg,
 			      u32 len);
 static int user_exp_rcv_invalid(struct hfi1_filedata *fd, unsigned long arg,
@@ -216,7 +220,11 @@ static long hfi1_file_ioctl(struct file *fp, unsigned int cmd,
 	case HFI1_IOCTL_TID_UPDATE_V3:
 		ret = user_exp_rcv_setup(fd, arg, _IOC_SIZE(cmd));
 		break;
-
+#ifdef NVIDIA_GPU_DIRECT
+	case HFI1_IOCTL_TID_UPDATE_V2:
+		ret = user_exp_rcv_setup_v2(fd, arg, _IOC_SIZE(cmd));
+		break;
+#endif
 	case HFI1_IOCTL_TID_FREE:
 		ret = user_exp_rcv_clear(fd, arg, _IOC_SIZE(cmd));
 		break;
@@ -1339,54 +1347,104 @@ static_assert(offsetof(struct hfi1_tid_info, length) == offsetof(struct hfi1_tid
 static_assert(offsetofend(struct hfi1_tid_info, length) == offsetofend(struct hfi1_tid_info_v3, length));
 
 /**
- * user_exp_rcv_setup - Set up the given tid rcv list
+ * _user_exp_rcv_setup - Set up the given tid rcv list
  * @fd: file data of the current driver instance
+ * @tinfo: already copied from userspace
  * @arg: ioctl argumnent for user space information
  * @len: length of data structure associated with ioctl command
+ * @allow_unaligned: when true, try to handle non-page-size multiple
+ *   (vaddr,length) in tidinfo from userspace.
  *
  * Wrapper to validate ioctl information before doing _rcv_setup.
  *
  */
-static int user_exp_rcv_setup(struct hfi1_filedata *fd, unsigned long arg,
-			      u32 len)
+static int _user_exp_rcv_setup(struct hfi1_filedata *fd, struct hfi1_tid_info_v3 *tinfo,
+			       unsigned long arg, u32 len, bool allow_unaligned)
 {
 	int ret;
 	unsigned long addr;
-	/* Zero out new fields for backwards compatibility with hfi1_tid_info */
-	struct hfi1_tid_info_v3 tinfo = {0};
-
-	if (copy_struct_from_user(&tinfo, sizeof(tinfo), (void __user *)arg, len))
-		return -EFAULT;
 
 	/* Reserved .flags bits must be 0 */
-	if (tinfo.flags & HFI1_TID_UPDATE_V3_FLAGS_RESERVED_MASK)
+	if (tinfo->flags & HFI1_TID_UPDATE_V3_FLAGS_RESERVED_MASK)
 		return -EINVAL;
 	/* Reserved for now */
-	if (tinfo.context)
+	if (tinfo->context)
 		return -EINVAL;
 
-	ret = hfi1_user_exp_rcv_setup(fd, &tinfo);
+	ret = hfi1_user_exp_rcv_setup(fd, tinfo, allow_unaligned);
 	if (!ret) {
 		/*
 		 * Copy the number of tidlist entries we used
 		 * and the length of the buffer we registered.
 		 */
 		addr = arg + offsetof(struct hfi1_tid_info, tidcnt);
-		if (copy_to_user((void __user *)addr, &tinfo.tidcnt,
-				 sizeof(tinfo.tidcnt)))
+		if (copy_to_user((void __user *)addr, &tinfo->tidcnt,
+				 sizeof(tinfo->tidcnt)))
 			ret = -EFAULT;
 
 		addr = arg + offsetof(struct hfi1_tid_info, length);
-		if (!ret && copy_to_user((void __user *)addr, &tinfo.length,
-					 sizeof(tinfo.length)))
+		if (!ret && copy_to_user((void __user *)addr, &tinfo->length,
+					 sizeof(tinfo->length)))
 			ret = -EFAULT;
 
 		if (ret)
-			hfi1_user_exp_rcv_invalid(fd, (struct hfi1_tid_info *)&tinfo);
+			hfi1_user_exp_rcv_invalid(fd, (struct hfi1_tid_info *)tinfo);
 	}
 
 	return ret;
 }
+
+/**
+ * Handles HFI1_IOCTL_TID_UPDATE and HFI1_IOCTL_TID_UPDATE_V3
+ */
+static int user_exp_rcv_setup(struct hfi1_filedata *fd, unsigned long arg,
+			      u32 len)
+{
+	struct hfi1_tid_info_v3 tinfo;
+
+	if (copy_struct_from_user(&tinfo, sizeof(tinfo), (void __user *)arg, len))
+		return -EFAULT;
+
+	return _user_exp_rcv_setup(fd, &tinfo, arg, len, false);
+}
+
+#ifdef NVIDIA_GPU_DIRECT
+
+/* Require that hfi1_tid_info_v2 is subset of hfi1_tid_info_v3 */
+static_assert(sizeof(struct hfi1_tid_info_v2) <= sizeof(struct hfi1_tid_info_v3));
+static_assert(offsetof(struct hfi1_tid_info_v2, flags) == offsetof(struct hfi1_tid_info_v3, flags));
+static_assert(offsetofend(struct hfi1_tid_info_v2, flags) <= offsetofend(struct hfi1_tid_info_v3, flags));
+
+/**
+ * Wrapper around _user_exp_rcv_setup() for HFI1_IOCTL_TID_UPDATE_V2.
+ */
+static int user_exp_rcv_setup_v2(struct hfi1_filedata *fd, unsigned long arg,
+				 u32 len)
+{
+	struct hfi1_tid_info_v3 tinfo;
+	struct hfi1_tid_info_v2 *tinfov2 = (struct hfi1_tid_info_v2 *)&tinfo;
+	int ret;
+
+	/*
+	 * offsetofend(struct hfi1_tid_info_v2,flags) <= offsetofend(struct hfi1_tid_info_v3,flags).
+	 * So copy_struct_from_user() is guaranteed to zero-fill everything
+	 * after struct hfi1_tid_info_v3.flags.
+	 */
+	ret = copy_struct_from_user(&tinfo, sizeof(tinfo), (void __user *)arg, len);
+	if (ret)
+		return ret;
+
+	/* Reserved .flags bits must be 0 */
+	if (tinfov2->flags & HFI1_TID_UPDATE_V2_FLAGS_RESERVED_MASK)
+		return -EINVAL;
+
+	/* Convert v2 .flags to v3 (.flags,.context) */
+	if ((tinfov2->flags & HFI1_TID_UPDATE_V2_FLAGS_GPU_MASK) == HFI1_BUF_GPU_MEM)
+		tinfo.flags = HFI1_MEMINFO_TYPE_NVIDIA;
+
+	return _user_exp_rcv_setup(fd, &tinfo, arg, len, true);
+}
+#endif
 
 /**
  * user_exp_rcv_clear - Clear the given tid rcv list

drivers/infiniband/hw/hfi1/tid_system.c
@@ -230,8 +230,12 @@ static bool sys_cover_invalidate(struct mmu_interval_notifier *mni,
 
 static struct tid_user_buf_ops sys_bufops;
 
+/*
+ * System memory never honors @allow_unaligned.
+ */
 static int sys_user_buf_init(u16 expected_count, bool notify,
 			     unsigned long vaddr, unsigned long length,
+			     bool allow_unaligned,
 			     struct tid_user_buf **tbuf)
 {
 	struct system_tid_user_buf *sbuf;

drivers/infiniband/hw/hfi1/trace.h
@@ -25,3 +25,6 @@ __print_symbolic(etype,                         \
 #ifdef CONFIG_HFI1_NVIDIA
 #include "trace_nvidia.h"
 #endif
+#ifdef NVIDIA_GPU_DIRECT
+#include "trace_gpu.h"
+#endif

drivers/infiniband/hw/hfi1/trace_gpu.h
@@ -1,0 +1,223 @@
+/* SPDX-License-Identifier: GPL-2.0 or BSD-3-Clause */
+/*
+ * Copyright(c) 2024 Cornelis Networks, Inc.
+ * Copyright(c) 2017 Intel Corporation.
+ *
+ * This file is provided under a dual BSD/GPLv2 license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * GPL LICENSE SUMMARY
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * BSD LICENSE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Intel Corporation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#if !defined(__HFI1_TRACE_GPU_H) || defined(TRACE_HEADER_MULTI_READ)
+#define __HFI1_TRACE_GPU_H
+
+#include <linux/tracepoint.h>
+#include <linux/trace_seq.h>
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM hfi1_gpu
+
+DECLARE_EVENT_CLASS(pin_gpu_pages,
+		    TP_PROTO(unsigned long base, unsigned long addr,
+			     unsigned long len, unsigned long size,
+			     unsigned int npages, void *arg),
+		    TP_ARGS(base, addr, len, size, npages, arg),
+		    TP_STRUCT__entry(__field(unsigned long, base)
+				     __field(unsigned long, addr)
+				     __field(unsigned long, len)
+				     __field(unsigned long, size)
+				     __field(unsigned int, npages)
+				     __field(void *, arg)
+			    ),
+		    TP_fast_assign(__entry->base = base;
+				   __entry->addr = addr;
+				   __entry->len = len;
+				   __entry->size = size;
+				   __entry->npages = npages;
+				   __entry->arg = arg;
+			    ),
+		    TP_printk("NV: arg: 0x%llx base: 0x%lx, addr: 0x%lx, len: %lu, pin size: %lu, num pages: %u",
+			      (unsigned long long)__entry->arg,
+			      __entry->base,
+			      __entry->addr,
+			      __entry->len,
+			      __entry->size,
+			      __entry->npages
+			    )
+);
+
+DECLARE_EVENT_CLASS(free_gpu_pages,
+		    TP_PROTO(unsigned long addr),
+		    TP_ARGS(addr),
+		    TP_STRUCT__entry(__field(unsigned long, addr)
+			    ),
+		    TP_fast_assign(__entry->addr = addr;
+			    ),
+		    TP_printk("NV: Unpinning the buffer at address: 0x%lx",
+			      __entry->addr
+			    )
+);
+
+DECLARE_EVENT_CLASS(invalidate_gpu_pages,
+		    TP_PROTO(void *arg, unsigned long addr, unsigned long size),
+		    TP_ARGS(arg, addr, size),
+		    TP_STRUCT__entry(__field(void *, arg)
+				     __field(unsigned long, addr)
+				     __field(unsigned long, size)
+			    ),
+		    TP_fast_assign(__entry->arg = arg;
+				   __entry->addr = addr;
+				   __entry->size = size;
+			    ),
+		    TP_printk("NV: arg %llx Invalidating address range: start 0x%lx, len %lu",
+			      (unsigned long long)__entry->arg,
+			      __entry->addr,
+			      __entry->size
+			    )
+);
+
+DECLARE_EVENT_CLASS(gpu_page_table_info,
+		    TP_PROTO(unsigned int entries, unsigned int page_size),
+		    TP_ARGS(entries, page_size),
+		    TP_STRUCT__entry(__field(unsigned int, entries)
+				     __field(unsigned int, page_size)
+			    ),
+		    TP_fast_assign(__entry->entries = entries;
+				   __entry->page_size = page_size;
+			    ),
+		    TP_printk("NV: nvidia_p2p_get_pages: num entries: %u, page size: %u",
+			      __entry->entries,
+			      __entry->page_size
+			    )
+);
+
+DECLARE_EVENT_CLASS(pin_gpu_pages_fail,
+		    TP_PROTO(int ret, unsigned long addr, unsigned long size),
+		    TP_ARGS(ret, addr, size),
+		    TP_STRUCT__entry(__field(int, ret)
+				     __field(unsigned long, addr)
+				     __field(unsigned long, size)
+			    ),
+		    TP_fast_assign(__entry->ret = ret;
+				   __entry->addr = addr;
+				   __entry->size = size;
+			    ),
+		    TP_printk("NV: Failed to pin GPU mem pages. ret %d, addr 0x%lx, size %lu",
+			      __entry->ret,
+			      __entry->addr,
+			      __entry->size
+			    )
+);
+
+TRACE_EVENT(gpu_page_size_check,
+	    TP_PROTO(unsigned int size_type),
+	    TP_ARGS(size_type),
+	    TP_STRUCT__entry(__field(unsigned int, size_type)),
+	    TP_fast_assign(__entry->size_type = size_type;),
+	    TP_printk("NV: GPU memory page size is not 64KB. Size type: %u",
+		      __entry->size_type)
+);
+
+TRACE_EVENT(gpu_page_tbl_check,
+	    TP_PROTO(unsigned int comp, unsigned int inst),
+	    TP_ARGS(comp, inst),
+	    TP_STRUCT__entry(__field(unsigned int, comp)
+			     __field(unsigned int, inst)
+			    ),
+	    TP_fast_assign(__entry->comp = comp;
+			   __entry->inst = inst;
+			  ),
+	    TP_printk("NV: page table version incompatible. Compiled: 0x%x, Installed: 0x%x",
+		      __entry->comp,
+		      __entry->inst)
+);
+
+DEFINE_EVENT(pin_gpu_pages, pin_rcv_pages_gpu,
+	     TP_PROTO(unsigned long base, unsigned long addr,
+		      unsigned long len, unsigned long size,
+		      unsigned int npages, void *arg),
+	     TP_ARGS(base, addr, len, size, npages, arg));
+
+DEFINE_EVENT(pin_gpu_pages, pin_sdma_pages_gpu,
+	     TP_PROTO(unsigned long base, unsigned long addr,
+		      unsigned long len, unsigned long size,
+		      unsigned int npages, void *arg),
+	     TP_ARGS(base, addr, len, size, npages, arg));
+
+DEFINE_EVENT(invalidate_gpu_pages, unpin_rcv_gpu_pages_callback,
+	     TP_PROTO(void *arg, unsigned long addr, unsigned long len),
+	     TP_ARGS(arg, addr, len));
+
+DEFINE_EVENT(invalidate_gpu_pages, unpin_gpu_pages_callback,
+	     TP_PROTO(void *arg, unsigned long addr, unsigned long len),
+	     TP_ARGS(arg, addr, len));
+
+DEFINE_EVENT(gpu_page_table_info, recv_gpu_page_table_info,
+	     TP_PROTO(unsigned int entries, unsigned int page_size),
+	     TP_ARGS(entries, page_size));
+
+DEFINE_EVENT(gpu_page_table_info, sdma_gpu_page_table_info,
+	     TP_PROTO(unsigned int entries, unsigned int page_size),
+	     TP_ARGS(entries, page_size));
+
+DEFINE_EVENT(pin_gpu_pages_fail, recv_pin_gpu_pages_fail,
+	     TP_PROTO(int ret, unsigned long addr, unsigned long size),
+	     TP_ARGS(ret, addr, size));
+
+DEFINE_EVENT(pin_gpu_pages_fail, sdma_pin_gpu_pages_fail,
+	     TP_PROTO(int ret, unsigned long addr, unsigned long size),
+	     TP_ARGS(ret, addr, size));
+
+DEFINE_EVENT(free_gpu_pages, free_recv_gpu_pages,
+	     TP_PROTO(unsigned long addr),
+	     TP_ARGS(addr));
+
+DEFINE_EVENT(free_gpu_pages, free_sdma_gpu_pages,
+	     TP_PROTO(unsigned long addr),
+	     TP_ARGS(addr));
+
+#endif /* __HFI1_TRACE_GPU_H */
+
+#undef TRACE_INCLUDE_PATH
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE trace_gpu
+#include <trace/define_trace.h>

drivers/infiniband/hw/hfi1/user_exp_rcv.c
@@ -155,7 +155,7 @@ static struct tid_node_ops *get_nodeops(u16 type)
  * @return 0 on success, non-zero on failure.
  */
 static int create_user_buf(struct hfi1_filedata *fd, u16 memtype, struct hfi1_tid_info_v3 *tinfo,
-			   struct tid_user_buf **ubuf)
+			   bool allow_unaligned, struct tid_user_buf **ubuf)
 {
 	struct hfi1_ctxtdata *uctxt = fd->uctxt;
 	struct tid_user_buf_ops *ops;
@@ -169,7 +169,7 @@ static int create_user_buf(struct hfi1_filedata *fd, u16 memtype, struct hfi1_ti
 		return -EINVAL;
 
 	ret = ops->init(uctxt->expected_count, fd->use_mn, tinfo->vaddr,
-			tinfo->length, ubuf);
+			tinfo->length, allow_unaligned, ubuf);
 	if (ret)
 		return ret;
 
@@ -226,7 +226,8 @@ static int create_user_buf(struct hfi1_filedata *fd, u16 memtype, struct hfi1_ti
  *          used, move it to tid_full_list.
  */
 int hfi1_user_exp_rcv_setup(struct hfi1_filedata *fd,
-			    struct hfi1_tid_info_v3 *tinfo)
+			    struct hfi1_tid_info_v3 *tinfo,
+			    bool allow_unaligned)
 {
 	int ret = 0, need_group = 0, pinned;
 	struct hfi1_ctxtdata *uctxt = fd->uctxt;
@@ -242,7 +243,7 @@ int hfi1_user_exp_rcv_setup(struct hfi1_filedata *fd,
 	u16 memtype = (tinfo->flags & HFI1_TID_UPDATE_V3_FLAGS_MEMINFO_MASK);
 
 	trace_hfi1_exp_tid_update(uctxt->ctxt, fd->subctxt, tinfo);
-	ret = create_user_buf(fd, memtype, tinfo, &tidbuf);
+	ret = create_user_buf(fd, memtype, tinfo, allow_unaligned, &tidbuf);
 	if (ret)
 		return ret;
 

drivers/infiniband/hw/hfi1/user_exp_rcv.h
@@ -114,6 +114,9 @@ struct tid_user_buf_ops {
 	 *   must return an error.
 	 * @vaddr
 	 * @length
+	 * @allow_unaligned when true, implementation may, but is not required
+	 *   to, handle unaligned @vaddr. When false, implementation must return
+	 *   -EINVAL for unaligned @vaddr.
 	 * @tbuf [out] allocated tid_user_buf
 	 *
 	 * @return 0 on success, non-zero on error.
@@ -129,6 +132,7 @@ struct tid_user_buf_ops {
 		    bool notify,
 		    unsigned long vaddr,
 		    unsigned long length,
+		    bool allow_unaligned,
 		    struct tid_user_buf **tbuf);
 
 	/*
@@ -329,7 +333,8 @@ int hfi1_user_exp_rcv_init(struct hfi1_filedata *fd,
 			   struct hfi1_ctxtdata *uctxt);
 void hfi1_user_exp_rcv_free(struct hfi1_filedata *fd);
 int hfi1_user_exp_rcv_setup(struct hfi1_filedata *fd,
-			    struct hfi1_tid_info_v3 *tinfo);
+			    struct hfi1_tid_info_v3 *tinfo,
+			    bool allow_unaligned);
 int hfi1_user_exp_rcv_clear(struct hfi1_filedata *fd,
 			    struct hfi1_tid_info *tinfo);
 int hfi1_user_exp_rcv_invalid(struct hfi1_filedata *fd,

commit b076ce15cdd077917abbd305d7b23f3a500ae25f

INTERNAL_GPU: Fix bad unaligned CUDA (vaddr,len) handling
HFI1_IOCTL_TID_UPDATE_V2 accepts non-page-aligned/non-page-multiple
("unaligned") CUDA (vaddr,len).

The CUDA unaligned (vaddr,len) handling code assumes that the difference
between (vaddr+len) and the next page boundary is always the same as
(vaddr & ~(NV_GPU_PAGE_MASK)). This assumption is not always true.

When this assumption is incorrect, the computed aligned length is wrong
and nvidia_p2p_get_pages() fails. When this call fails, it may produce a
kernel message like so:

    [  542.084270] NVRM: Invalid argument in nv_p2p_get_pages,address or length are not aligned address=0x7f12b2800000, length=0x38000

Fix this by computing the end based on the unadjusted start vaddr and
length passed in from userspace and align that to the next
NV_GPU_PAGE_SIZE boundary.

INTERNAL: STL-69821 hfi1-user-tests/test-user-tid
INTERNAL: test_tid_v2_unaligned_start fails with EINVAL

Fixes: 5189b5ba952a ("INTERNAL_GPU: Add HFI1_IOCTL_TID_UPDATE_V2 for PSM2-CUDA")
Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
(cherry picked from commit e764e642ab5c41a73437bc79c1ae051c547527b6)

Conflicts:
	drivers/infiniband/hw/hfi1/tid_nvidia.c

drivers/infiniband/hw/hfi1/tid_nvidia.c
@@ -60,7 +60,6 @@
 
 #define NV_GPU_PAGE_SHIFT 16
 #define NV_GPU_PAGE_SIZE BIT(NV_GPU_PAGE_SHIFT)
-#define NV_GPU_PAGE_MASK (~(NV_GPU_PAGE_SIZE - 1))
 
 #define GPU_PAGE_TO_PFN(page) ((page)->physical_address >> NV_GPU_PAGE_SHIFT)
 
@@ -264,14 +263,13 @@ static int nvidia_user_buf_init(u16 expected_count,
 				struct tid_user_buf **tbuf)
 {
 	struct nvidia_tid_user_buf *nvbuf;
-	unsigned long unaligned_vaddr = vaddr;
 	int ret;
 
 	/* Allow non-page-size multiple (vaddr,length) for older userspace. */
 	if (!allow_unaligned) {
 		if (!IS_ALIGNED(vaddr, max(EXPECTED_ADDR_SIZE, NV_GPU_PAGE_SIZE)))
 			return -EINVAL;
-		if (length % NV_GPU_PAGE_SIZE)
+		if (!IS_ALIGNED(vaddr + length, NV_GPU_PAGE_SIZE))
 			return -EINVAL;
 	}
 
@@ -290,8 +288,10 @@ static int nvidia_user_buf_init(u16 expected_count,
 	 */
 	nvbuf->orig_vaddr = vaddr;
 	nvbuf->orig_length = length;
-	vaddr = (vaddr & NV_GPU_PAGE_MASK);
-	length += (unaligned_vaddr - vaddr);
+	/* Align vaddr to page boundary if it wasn't already */
+	vaddr = ALIGN_DOWN(vaddr, NV_GPU_PAGE_SIZE);
+	/* Compute end based on original unaligned vaddr, not adjusted vaddr */
+	length = ALIGN(nvbuf->orig_vaddr + length, NV_GPU_PAGE_SIZE) - vaddr;
 	nvbuf->common.vaddr = vaddr;
 	nvbuf->common.length = length;
 	nvbuf->common.use_mn = notify;