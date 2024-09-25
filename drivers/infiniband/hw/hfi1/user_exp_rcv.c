cornelisnetworks/opa-distro-drivers

commit 15df83f2cfadd496198d7896f5eea7013d9afa95

INTERNAL_GPU: user-expected-receive code cleanup
Changes:
- Move set_rcvarray_entry() definition to eliminate need for forward
  declaration.
- Change tid_node_ops.init() to use ERR_PTR.
- Make newlines before/after 'return' and label lines consistent.
- user_exp_rcv.h: Replace '/*' with '/**' comments for struct fields.
- Use tid_user_buf_init() and tid_user_buf_free() for allocating/freeing
  'common' user buf data in tid_system.c.
- Change nvidia_node_init() to return ptr/ERR_PTR.
- Shorten local variable names.
- Make newlines before/after 'return' and label lines consistent.
- Use tid_user_buf_init() and tid_user_buf_free() for allocating/freeing
  'common' user buf data in tid_system.c and tid_nvidia.c.

INTERNAL: STL-67108 TID Receive support for AMD memory

Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
(cherry picked from commit e757e720d91eebca36e7441e9b2d54061e55a89c)
(cherry picked from commit 3f09fc5ba3a5c26399942bbb1adf80c4f4af0dc6)
(cherry picked from commit 5cc21e39048de0fb9a05b7267f333368bbe222d9)
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

drivers/infiniband/hw/hfi1/user_exp_rcv.c
@@ -13,11 +13,6 @@
 static void unlock_exp_tids(struct hfi1_ctxtdata *uctxt,
 			    struct exp_tid_set *set,
 			    struct hfi1_filedata *fd);
-static int set_rcvarray_entry(struct hfi1_filedata *fd,
-			      struct tid_user_buf *tbuf,
-			      u32 rcventry, struct tid_group *grp,
-			      struct tid_pageset pgset,
-			      struct tid_rb_node **onode);
 static void cacheless_tid_rb_remove(struct hfi1_filedata *fdata,
 				    struct tid_rb_node *tnode);
 static int program_rcvarray(struct hfi1_filedata *fd,
@@ -147,6 +142,26 @@ static struct tid_node_ops *get_nodeops(u16 type)
 	if (type >= HFI1_MAX_MEMINFO_ENTRIES)
 		return NULL;
 	return nodeops[type];
+}
+
+int tid_user_buf_init(u16 pset_size, unsigned long vaddr, unsigned long length, bool notify,
+		      struct tid_user_buf_ops *ops, u16 type, struct tid_user_buf *tbuf)
+{
+	tbuf->vaddr = vaddr;
+	tbuf->length = length;
+	tbuf->use_mn = notify;
+	tbuf->psets = kcalloc(pset_size, sizeof(*tbuf->psets), GFP_KERNEL);
+	if (!tbuf->psets)
+		return -ENOMEM;
+	tbuf->ops = ops;
+	tbuf->type = type;
+	return 0;
+}
+
+void tid_user_buf_free(struct tid_user_buf *tbuf)
+{
+	kfree(tbuf->psets);
+	tbuf->psets = NULL;
 }
 
 /**
@@ -543,6 +558,67 @@ static unsigned int node_npages(const struct tid_rb_node *node)
 	return (node->npages << node->page_shift) >> EXPECTED_ADDR_SHIFT;
 }
 
+/*
+ * DMA-map and program single TID entry for physically contiguous pinned page
+ * range.
+ *
+ * @fd
+ * @tbuf
+ * @rcventry
+ * @grp
+ * @pgset Gives page-range from @tbuf to DMA-map and program
+ * @onode out node. Undefined on error.
+ *
+ * @return 0 on success, non-zero on error.
+ */
+static int set_rcvarray_entry(struct hfi1_filedata *fd,
+			      struct tid_user_buf *tbuf,
+			      u32 rcventry, struct tid_group *grp,
+			      struct tid_pageset pgset,
+			      struct tid_rb_node **onode)
+{
+	struct hfi1_ctxtdata *uctxt = fd->uctxt;
+	struct tid_rb_node *node;
+	struct hfi1_devdata *dd = uctxt->dd;
+	struct tid_node_ops *nodeops = get_nodeops(tbuf->type);
+	unsigned int npages = pgset.count;
+	u16 pageidx = pgset.idx;
+	int ret;
+
+	if (WARN_ON(!nodeops))
+		return -EINVAL;
+
+	node = nodeops->init(fd, tbuf, rcventry, grp, pageidx, npages);
+	if (IS_ERR(node))
+		return PTR_ERR(node);
+
+	if (node->use_mn) {
+		ret = node->ops->register_notify(node);
+		if (ret)
+			goto out_unmap;
+	}
+	*onode = node;
+	fd->entry_to_rb[node->rcventry] = node;
+
+	/* RcvArray entry requires EXPECTED_ADDR_SIZE page-size npages */
+	hfi1_put_tid(dd, rcventry, PT_EXPECTED, node->dma_addr, ilog2(node_npages(node)) + 1);
+
+	trace_hfi1_exp_tid_reg(uctxt->ctxt, fd->subctxt, rcventry,
+			       node_npages(node),
+			       node->vaddr, node->phys,
+			       node->dma_addr, node->type);
+	return 0;
+
+out_unmap:
+	hfi1_cdbg(TID, "Failed to insert RB node %u 0x%lx, 0x%lx %d",
+		  node->rcventry, node->vaddr, node->phys, ret);
+
+	node->ops->dma_unmap(node);
+	node->ops->free(node);
+
+	return -EFAULT;
+}
+
 /**
  * program_rcvarray() - program an RcvArray group with receive buffers
  * @fd: filedata pointer
@@ -633,68 +709,6 @@ static int program_rcvarray(struct hfi1_filedata *fd,
 		rcv_array_wc_fill(dd, grp->base + useidx);
 	*pmapped = mapped;
 	return idx;
-}
-
-/*
- * DMA-map and program single TID entry for physically contiguous pinned page
- * range.
- *
- * @fd
- * @tbuf
- * @rcventry
- * @grp
- * @pgset Gives page-range from @tbuf to DMA-map and program
- * @onode out node. Undefined on error.
- *
- * @return 0 on success, non-zero on error.
- */
-static int set_rcvarray_entry(struct hfi1_filedata *fd,
-			      struct tid_user_buf *tbuf,
-			      u32 rcventry, struct tid_group *grp,
-			      struct tid_pageset pgset,
-			      struct tid_rb_node **onode)
-{
-	int ret;
-	struct hfi1_ctxtdata *uctxt = fd->uctxt;
-	struct tid_rb_node *node;
-	struct hfi1_devdata *dd = uctxt->dd;
-	struct tid_node_ops *nodeops = get_nodeops(tbuf->type);
-	unsigned int npages = pgset.count;
-	u16 pageidx = pgset.idx;
-
-	if (WARN_ON(!nodeops))
-		return -EINVAL;
-
-	ret = nodeops->init(fd, tbuf, rcventry, grp, pageidx, npages, &node);
-	if (ret)
-		return ret;
-
-	if (node->use_mn) {
-		ret = node->ops->register_notify(node);
-		if (ret)
-			goto out_unmap;
-	}
-
-	*onode = node;
-	fd->entry_to_rb[node->rcventry - uctxt->expected_base] = node;
-
-	/* RcvArray entry requires EXPECTED_ADDR_SIZE page-size npages */
-	hfi1_put_tid(dd, rcventry, PT_EXPECTED, node->dma_addr,
-		     ilog2(node_npages(node)) + 1);
-
-	trace_hfi1_exp_tid_reg(uctxt->ctxt, fd->subctxt, rcventry,
-			       node_npages(node),
-			       node->vaddr, node->phys,
-			       node->dma_addr, node->type);
-	return 0;
-
-out_unmap:
-	hfi1_cdbg(TID, "Failed to insert RB node %u 0x%lx, 0x%lx %d",
-		  node->rcventry, node->vaddr, node->phys, ret);
-
-	node->ops->dma_unmap(node);
-	node->ops->free(node);
-	return -EFAULT;
 }
 
 static int unprogram_rcvarray(struct hfi1_filedata *fd, u32 tidinfo)

drivers/infiniband/hw/hfi1/tid_nvidia.c
@@ -157,46 +157,38 @@ static unsigned int nvidia_pgt_shift(const struct nvidia_p2p_page_table *pages)
 
 static struct tid_node_ops nvidia_nodeops;
 
-static int nvidia_node_init(struct hfi1_filedata *fd,
-			    struct tid_user_buf *tbuf,
-			    u32 rcventry,
-			    struct tid_group *grp,
-			    u16 pageidx,
-			    unsigned int npages,
-			    struct tid_rb_node **node)
+static struct tid_rb_node *nvidia_node_init(struct hfi1_filedata *fd, struct tid_user_buf *tbuf,
+					    u32 rcventry, struct tid_group *grp, u16 pageidx,
+					    unsigned int npages)
 {
 	struct nvidia_tid_user_buf *nvbuf =
 		container_of(tbuf, struct nvidia_tid_user_buf, common);
 	struct nvidia_tid_node *nvnode;
-	unsigned int page_shift;
+	unsigned int ps;
 	u32 pgsz;
-	int ret = 0;
 
 	/* As long as fd is passed in separately, sanity-check */
 	if (nvbuf->fd != fd)
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 	if (!nvbuf->pages || !nvbuf->mapping)
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 	if (pageidx > nvbuf->pages->entries ||
 	    (pageidx + npages) > nvbuf->pages->entries)
-		return -EINVAL;
+		return ERR_PTR(-EINVAL);
 
-	page_shift = nvidia_pgt_shift(nvbuf->pages);
+	ps = nvidia_pgt_shift(nvbuf->pages);
 	/* Only 64KiB pages supported right now */
-	if (page_shift != NV_GPU_PAGE_SHIFT)
-		return -EIO;
-	pgsz = 1 << page_shift;
+	if (ps != NV_GPU_PAGE_SHIFT)
+		return ERR_PTR(-EIO);
+	pgsz = 1 << ps;
 
 	nvnode = kzalloc(sizeof(*nvnode), GFP_KERNEL);
 	if (!nvnode)
-		return -ENOMEM;
+		return ERR_PTR(-ENOMEM);
 
 	spin_lock(&nvbuf->nodes_lock);
-	if (nvbuf->invalidated) {
-		ret = -EFAULT;
+	if (nvbuf->invalidated)
 		goto unlock_free;
-	}
-	*node = &nvnode->common;
 
 	kref_get(&nvbuf->ref);
 	nvnode->userbuf = nvbuf;
@@ -208,7 +200,7 @@ static int nvidia_node_init(struct hfi1_filedata *fd,
 	nvnode->common.rcventry = rcventry;
 	nvnode->pageidx = pageidx;
 	nvnode->common.npages = npages;
-	nvnode->common.page_shift = page_shift;
+	nvnode->common.page_shift = ps;
 	nvnode->common.phys = nvbuf->pages->pages[pageidx]->physical_address;
 	nvnode->common.dma_addr = nvbuf->mapping->dma_addresses[pageidx];
 	nvnode->common.vaddr = tbuf->vaddr + (pageidx * pgsz);
@@ -218,13 +210,12 @@ static int nvidia_node_init(struct hfi1_filedata *fd,
 	list_add_tail(&nvnode->list, &nvbuf->nodes);
 	spin_unlock(&nvbuf->nodes_lock);
 
-	return 0;
-
+	return &nvnode->common;
 unlock_free:
 	spin_unlock(&nvbuf->nodes_lock);
 	kfree(nvnode);
 
-	return ret;
+	return ERR_PTR(-EFAULT);
 }
 
 static void nvidia_node_free(struct tid_rb_node *node)
@@ -265,7 +256,7 @@ static int nvidia_user_buf_init(u16 expected_count,
 	struct nvidia_tid_user_buf *nvbuf;
 	int ret;
 
-	/* Allow non-page-size multiple (vaddr,length) for older userspace. */
+	/* Allow non-page-size multiple (vaddr,length) for older userspace */
 	if (!allow_unaligned) {
 		if (!IS_ALIGNED(vaddr, max(EXPECTED_ADDR_SIZE, NV_GPU_PAGE_SIZE)))
 			return -EINVAL;
@@ -276,39 +267,31 @@ static int nvidia_user_buf_init(u16 expected_count,
 	nvbuf = kzalloc(sizeof(*nvbuf), GFP_KERNEL);
 	if (!nvbuf)
 		return -ENOMEM;
+
 	kref_init(&nvbuf->ref);
 	*tbuf = &nvbuf->common;
 
-	/*
-	 * In case vaddr is not aligned to a page boundary,
-	 * round vaddr off to a page boundary and increase
-	 * length to cover the difference.
-	 *
-	 * Store original vaddr, length on nvbuf for tracing.
-	 */
+	/* Store original vaddr, length on nvbuf for tracing */
 	nvbuf->orig_vaddr = vaddr;
 	nvbuf->orig_length = length;
 	/* Align vaddr to page boundary if it wasn't already */
 	vaddr = ALIGN_DOWN(vaddr, NV_GPU_PAGE_SIZE);
 	/* Compute end based on original unaligned vaddr, not adjusted vaddr */
 	length = ALIGN(nvbuf->orig_vaddr + length, NV_GPU_PAGE_SIZE) - vaddr;
-	nvbuf->common.vaddr = vaddr;
-	nvbuf->common.length = length;
-	nvbuf->common.use_mn = notify;
-	nvbuf->common.psets = kcalloc(expected_count, sizeof(*nvbuf->common.psets),
-				      GFP_KERNEL);
-	if (!nvbuf->common.psets) {
-		ret = -ENOMEM;
+
+	ret = tid_user_buf_init(expected_count, vaddr, length, notify, &nvidia_bufops,
+				HFI1_MEMINFO_TYPE_NVIDIA, *tbuf);
+	if (ret)
 		goto fail_release_mem;
-	}
-	nvbuf->common.ops = &nvidia_bufops;
-	nvbuf->common.type = HFI1_MEMINFO_TYPE_NVIDIA;
+
 	INIT_LIST_HEAD(&nvbuf->nodes);
 	spin_lock_init(&nvbuf->nodes_lock);
+
 	return 0;
 fail_release_mem:
-	// No need to do kref_put here, just kfree()
+	/* No need to do kref_put here, just kfree() */
 	kfree(nvbuf);
+
 	return ret;
 }
 
@@ -340,7 +323,7 @@ static void nvidia_user_buf_kref_cb(struct kref *ref)
 	 */
 	if (nvbuf->pages)
 		WARN_ON(rdma_interface.put_pages(0, 0, nvbuf->common.vaddr, nvbuf->pages));
-	kfree(nvbuf->common.psets);
+	tid_user_buf_free(&nvbuf->common);
 	kfree(nvbuf);
 }
 
@@ -424,7 +407,6 @@ static int nvidia_pin_pages(struct hfi1_filedata *fd,
 	}
 
 	return nvbuf->pages->entries;
-
 fail_unmap:
 	WARN_ON(rdma_interface.dma_unmap_pages(fd->dd->pcidev, nvbuf->pages, nvbuf->mapping));
 	nvbuf->mapping = NULL;
@@ -432,6 +414,7 @@ fail_put_pages:
 	WARN_ON(rdma_interface.put_pages(0, 0, nvbuf->common.vaddr, nvbuf->pages));
 	nvbuf->pages = NULL;
 	trace_recv_pin_gpu_pages_fail(ret, nvbuf->common.vaddr, nvbuf->common.length);
+
 	return ret;
 }
 
@@ -529,6 +512,7 @@ static bool nvidia_invalidated(struct tid_user_buf *tbuf)
 	spin_lock(&nvbuf->nodes_lock);
 	ret = nvbuf->invalidated;
 	spin_unlock(&nvbuf->nodes_lock);
+
 	return ret;
 }
 
@@ -578,10 +562,12 @@ int register_nvidia_tid_ops(void)
 	if (ret)
 		goto fail;
 	pr_info("%s Nvidia p2p TID-DMA support enabled\n", class_name());
+
 	return 0;
 fail:
 	deregister_nvidia_tid_ops();
 	pr_info("%s Nvidia p2p TID-DMA support disabled\n", class_name());
+
 	return ret;
 }
 

drivers/infiniband/hw/hfi1/tid_system.c
@@ -135,20 +135,19 @@ static void sys_node_unpin_pages(struct hfi1_filedata *fd,
 
 static struct tid_node_ops sys_nodeops;
 
-static int sys_node_init(struct hfi1_filedata *fd,
-			 struct tid_user_buf *tbuf,
-			 u32 rcventry,
-			 struct tid_group *grp,
-			 u16 pageidx,
-			 unsigned int npages,
-			 struct tid_rb_node **node)
+static struct tid_rb_node *sys_node_init(struct hfi1_filedata *fd,
+					 struct tid_user_buf *tbuf,
+					 u32 rcventry,
+					 struct tid_group *grp,
+					 u16 pageidx,
+					 unsigned int npages)
 {
 	struct system_tid_user_buf *sbuf =
 		container_of(tbuf, struct system_tid_user_buf, common);
 	struct hfi1_devdata *dd = fd->uctxt->dd;
-	dma_addr_t phys;
 	struct page **pages = sbuf->pages + pageidx;
 	struct system_tid_node *snode;
+	dma_addr_t phys;
 
 	/*
 	 * Allocate snode first so we can handle a potential failure before
@@ -156,16 +155,15 @@ static int sys_node_init(struct hfi1_filedata *fd,
 	 */
 	snode = kzalloc(struct_size(snode, pages, npages), GFP_KERNEL);
 	if (!snode)
-		return -ENOMEM;
+		return ERR_PTR(-ENOMEM);
 
-	*node = &snode->common;
 	phys = dma_map_single(&dd->pcidev->dev, __va(page_to_phys(pages[0])),
 			      npages * PAGE_SIZE, DMA_FROM_DEVICE);
 	if (dma_mapping_error(&dd->pcidev->dev, phys)) {
 		dd_dev_err(dd, "Failed to DMA map Exp Rcv pages 0x%llx\n",
 			   phys);
 		kfree(snode);
-		return -EFAULT;
+		return ERR_PTR(-EFAULT);
 	}
 
 	snode->common.fdata = fd;
@@ -183,7 +181,7 @@ static int sys_node_init(struct hfi1_filedata *fd,
 	snode->common.type = HFI1_MEMINFO_TYPE_SYSTEM;
 	memcpy(snode->pages, pages, flex_array_size(snode, pages, npages));
 
-	return 0;
+	return &snode->common;
 }
 
 static void sys_node_free(struct tid_rb_node *node)
@@ -249,21 +247,15 @@ static int sys_user_buf_init(u16 expected_count, bool notify,
 		return -ENOMEM;
 	*tbuf = &sbuf->common;
 	mutex_init(&sbuf->cover_mutex);
-	sbuf->common.vaddr = vaddr;
-	sbuf->common.length = length;
-	sbuf->common.use_mn = notify;
-	sbuf->common.psets = kcalloc(expected_count, sizeof(*sbuf->common.psets),
-				     GFP_KERNEL);
-	if (!sbuf->common.psets) {
-		ret = -ENOMEM;
+
+	ret = tid_user_buf_init(expected_count, vaddr, length, notify, &sys_bufops,
+				HFI1_MEMINFO_TYPE_SYSTEM, *tbuf);
+	if (ret)
 		goto fail_release_mem;
-	}
-	sbuf->common.ops = &sys_bufops;
-	sbuf->common.type = HFI1_MEMINFO_TYPE_SYSTEM;
+
 	sbuf->npages = num_user_pages(vaddr, length);
 
 	return 0;
-
 fail_release_mem:
 	sys_user_buf_free(&sbuf->common);
 	return ret;
@@ -275,7 +267,7 @@ static void sys_user_buf_free(struct tid_user_buf *tbuf)
 		container_of(tbuf, struct system_tid_user_buf, common);
 
 	kfree(sbuf->pages);
-	kfree(sbuf->common.psets);
+	tid_user_buf_free(tbuf);
 	kfree(sbuf);
 }
 

drivers/infiniband/hw/hfi1/user_exp_rcv.h
@@ -59,9 +59,14 @@ struct tid_user_buf {
 	u16 type; /* Implementation must set to HFI1_MEMINFO_TYPE* in tid_user_buf_ops.init() */
 };
 
+int tid_user_buf_init(u16 pset_size, unsigned long vaddr, unsigned long length, bool notify,
+		      struct tid_user_buf_ops *ops, u16 type, struct tid_user_buf *tbuf);
+void tid_user_buf_free(struct tid_user_buf *tbuf);
+
 struct tid_rb_node {
 	struct hfi1_filedata *fdata;
 	struct mutex invalidate_mutex; /* covers hw removal */
+	/* Only used for debug and tracing */
 	unsigned long phys;
 	struct tid_group *grp;
 	struct tid_node_ops *ops;
@@ -93,7 +98,7 @@ struct tid_rb_node {
  * pages are pinned.
  */
 struct tid_user_buf_ops {
-	/*
+	/**
 	 * Allocate and initialize @*tbuf.
 	 *
 	 * Implementation must initialize:
@@ -135,12 +140,12 @@ struct tid_user_buf_ops {
 		    bool allow_unaligned,
 		    struct tid_user_buf **tbuf);
 
-	/*
+	/**
 	 * Free @tbuf.
 	 */
 	void (*free)(struct tid_user_buf *tbuf);
 
-	/*
+	/**
 	 * Pin pages for @tbuf based on (@vaddr,@length) passed into
 	 * @tid_user_buf_ops.init().
 	 *
@@ -156,7 +161,7 @@ struct tid_user_buf_ops {
 	 */
 	int (*pin_pages)(struct hfi1_filedata *fd, struct tid_user_buf *tbuf);
 
-	/*
+	/**
 	 * Get page size of @tbuf's pinned pages.
 	 *
 	 * Caller should not assume that page size is known until after
@@ -168,7 +173,7 @@ struct tid_user_buf_ops {
 	 */
 	unsigned int (*page_size)(struct tid_user_buf *tbuf);
 
-	/*
+	/**
 	 * Unpin only @npages starting at @idx.
 	 *
 	 * Implementation may implement partial unpinning.
@@ -186,7 +191,7 @@ struct tid_user_buf_ops {
 			    unsigned int idx,
 			    unsigned int npages);
 
-	/*
+	/**
 	 * Implementation must program @tbuf->psets elements such that:
 	 * 1. All pages in a pageset are physically contiguous
 	 * 2. All pages in all pagesets have the same page size
@@ -205,14 +210,14 @@ struct tid_user_buf_ops {
 	int (*find_phys_blocks)(struct tid_user_buf *tbuf,
 				unsigned int npages);
 
-	/*
+	/**
 	 * @return true when:
 	 * - @tbuf's virtual->physical mapping has been invalidated
 	 * - @tbuf's physical pages have been released
 	 */
 	bool (*invalidated)(struct tid_user_buf *tbuf);
 
-	/*
+	/**
 	 * Unregister memory-invalidation callback registered in struct
 	 * tid_user_buf_ops.init() implementation.
 	 */
@@ -220,7 +225,7 @@ struct tid_user_buf_ops {
 };
 
 struct tid_node_ops {
-	/*
+	/**
 	 * Create tid_rb_node for pinned-page range. Page range given by
 	 * (@pageidx,@npages). Pinned pages are in @tbuf.
 	 *
@@ -251,24 +256,22 @@ struct tid_node_ops {
 	 * @grp
 	 * @pageidx
 	 * @npages
-	 * @node [out] allocated node
 	 *
-	 * @ return 0 on success, non-zero on failure.
+	 * @return allocated node on success, ERR_PTR() on error.
 	 */
-	int (*init)(struct hfi1_filedata *fd,
-		    struct tid_user_buf *tbuf,
-		    u32 rcventry,
-		    struct tid_group *grp,
-		    u16 pageidx,
-		    unsigned int npages,
-		    struct tid_rb_node **node);
+	struct tid_rb_node *(*init)(struct hfi1_filedata *fd,
+				    struct tid_user_buf *tbuf,
+				    u32 rcventry,
+				    struct tid_group *grp,
+				    u16 pageidx,
+				    unsigned int npages);
 
-	/*
+	/**
 	 * Free @node.
 	 */
 	void (*free)(struct tid_rb_node *node);
 
-	/*
+	/**
 	 * Register for memory invalidation callback. Implementation should
 	 * only register for notification on @node's page-range.
 	 *
@@ -285,7 +288,7 @@ struct tid_node_ops {
 	 */
 	int (*register_notify)(struct tid_rb_node *node);
 
-	/*
+	/**
 	 * Unregister from memory-invalidation callback in anticipation of
 	 * unprogramming TID.
 	 *
@@ -296,7 +299,7 @@ struct tid_node_ops {
 	 */
 	void (*unregister_notify)(struct tid_rb_node *node);
 
-	/*
+	/**
 	 * DMA-unmap mapped memory for @node.
 	 *
 	 * Should be no-op when implementation does not support partial
@@ -304,7 +307,7 @@ struct tid_node_ops {
 	 */
 	void (*dma_unmap)(struct tid_rb_node *node);
 
-	/*
+	/**
 	 * Unpin pages covered by @node.
 	 *
 	 * user_exp_rcv will call this function under node->invalidate_mutex.

commit d11b17fe6699c55a5debe79ace8d77e1c449c177

RDMA/hfi1: fix bad index calculation in set_rcvarray_entry()
Commit 15df83f2cfad("INTERNAL_GPU: user-expected-receive code cleanup")
introduced an incorrect index calculation in user_exp_rcv.c
set_rcvarray_entry() when indexing into
struct hfi1_filedata->entry_to_rb[].

As a result, the struct tid_rb_node object created by an
HFI1_IOCTL_TID_UPDATE ioctl() was stored at the wrong location in memory
and subsequent user-expected receive operations (HFI1_IOCTL_TID_FREE,
user-expected receive destructor code at process teardown) either failed
to find the struct tid_rb_node object or dereferenced a null pointer,
causing a kernel panic.

Fix this by using the correct calculation that offsets node->rcventry by
uctxt->expected_base.

INTERNAL: STL-70329 PSM2 failures in OPXS 10.14.4.0.4 - "Failed to tid
INTERNAL: free 30 tids (err=23)" followed by kernel oops and crash

Fixes: 15df83f2cfad("INTERNAL_GPU: user-expected-receive code cleanup")
Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

drivers/infiniband/hw/hfi1/user_exp_rcv.c
@@ -722,7 +722,7 @@ static int set_rcvarray_entry(struct hfi1_filedata *fd,
 			goto out_unmap;
 	}
 	*onode = node;
-	fd->entry_to_rb[node->rcventry] = node;
+	fd->entry_to_rb[node->rcventry - uctxt->expected_base] = node;
 
 	/* RcvArray entry requires EXPECTED_ADDR_SIZE page-size npages */
 	hfi1_put_tid(dd, rcventry, PT_EXPECTED, node->dma_addr, ilog2(node_npages(node)) + 1);