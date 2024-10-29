fb0a420e217bf5fcf60a1dbd96bfbf7350ae432c
Douglas Miller <doug.miller@cornelisnetworks.com>
2024-10-29 08:35:57 -0400

    RDMA/hfi1: Add base support for SRIOV
    Handle minimal setup of SRIOV on the host only. Probe on VM will only
    dump basic PCI device info.
    
    Module param "vf_test" may be set 'true' to cause the host VF PCI
    device to be enabled (PCI_COMMAND_MEMORY) and set to PCI_COMMAND_MASTER.
    
    Module param "vf_claim" may be set 'true' to cause the host VF PCI
    device to be claimed by the driver, and the host driver will attempt
    to initialize the device.
    
    Signed-off-by: Douglas Miller <doug.miller@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/chip_gen.h
+++ drivers/infiniband/hw/hfi1/chip_gen.h
@@ -22,6 +22,7 @@ u64 gen_create_pbc(struct hfi1_pportdata *ppd, u64 flags, int srate_mbs, u32 vl,
 int cport_set_link_state(struct hfi1_pportdata *ppd, struct opa_port_info *pi, u32 state);
 int cport_start_link(struct hfi1_pportdata *ppd, struct opa_port_info *pi);
 int cport_read_temp(struct hfi1_devdata *dd, struct cport_temp *gen_temp);
+int hfi1_sriov_sync_ports(struct hfi1_devdata *dd, int si_mask);
 
 int init_cport_trap128(struct hfi1_devdata *dd);
 int deinit_cport_trap128(struct hfi1_devdata *dd);
