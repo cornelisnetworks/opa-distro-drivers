820816e5943358cae3928ed0c71eceb1515ab179
Brendan Cunningham <bcunningham@cornelisnetworks.com>
2025-08-21 09:33:30 -0400

    RDMA/hfi1: set DMA mask width per HFI chip-type
    WFR supports 48-bit DMA addresses while JKR supports 58-bit DMA
    addresses. However, pcie.c calls dma_set_mask_and_coherent() with
    DMA_BIT_MASK(64).
    
    As a result, dma_map*() and dma_alloc*() calls can return DMA addresses
    outside the supported range for either WFR or JKR.
    
    Fix this by calling dma_set_mask_and_coherent() with a per chip-type DMA
    mask-size. Additionally, move the dma_set_mask_and_coherent() call to
    hfi1_alloc_devdata(), before any dma_map*() or dma_alloc*() calls have
    been made for the device. This closes a hole where some hfi1_devdata
    fields could get a DMA address that was incompatible with the HFI.
    
    INTERNAL: STL-72095 Use ASIC-specific CSR value-masks for fields that
    INTERNAL: differ by ASIC
    
    Signed-off-by: Brendan Cunningham <bcunningham@cornelisnetworks.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/pcie.c
+++ drivers/infiniband/hw/hfi1/pcie.c
@@ -81,20 +81,6 @@ int hfi1_pcie_init(struct hfi1_devdata *dd)
 		goto bail;
 	}
 
-	ret = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64));
-	if (ret) {
-		/*
-		 * If the 64 bit setup fails, try 32 bit.  Some systems
-		 * do not setup 64 bit maps on systems with 2GB or less
-		 * memory installed.
-		 */
-		ret = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
-		if (ret) {
-			dd_dev_err(dd, "Unable to set DMA mask: %d\n", ret);
-			goto bail;
-		}
-	}
-
 	pci_set_master(pdev);
 	if (dd->params->chip_type == CHIP_JKR)
 		mask_aer_unsupported_request(pdev);
