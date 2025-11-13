364e6f906c4ec8e6a7d7c221859af6bbf58e7073
Douglas Miller <doug.miller@cornelisnetworks.com>
2025-11-13 14:55:20 -0500

    RDMA/hfi1: Harden CPORT MCTXT state recovery
    Detect MCTXT PF0 CSRs in a bad state and attempt to repair.  This is
    controlled by module param 'cport_mctxt_recovery' which defaults to
    'true'.
    
    Use a timeout on the initial PING to avoid hangs trying to
    flush the WQ on START failure. Add a module param 'cport_adm_to' to
    allow configuration of the timeout used for all administrative (non-MAD)
    CPORT messages, including the initial PING.
    
    Do a STOP before initial START when probing the device, to try and
    reset resources. The subsequent START uses a larger timeout to allow
    the firmware to perform all the tasks for the STOP.
    
    INTERNAL: STL-75182
    
    Signed-off-by: Douglas Miller <doug.miller@cornelisnetworks.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/mad.c
+++ drivers/infiniband/hw/hfi1/mad.c
@@ -5215,7 +5215,7 @@ int cport_send_only_mad(struct hfi1_devdata *dd, u8 sb, const void *mad, int len
 	memcpy(buf + MAD_9B_OFFSET, mad, size - MAD_9B_OFFSET);
 	/* XXX hard-code DETH.SrcQP=2 for firmware */
 	((struct ib_header *)buf)->u.oth.u.ud.deth[1] = cpu_to_be32(2);
-	ret = cport_send_notif(dd, CH_OP_MAD_9B, sb, buf, size);
+	ret = cport_send_notif(dd, CH_OP_MAD_9B, sb, buf, size, cport_adm_to * HZ);
 	kfree(buf);
 	return ret;
 }
