9648af57ba13b82d88720d0fa2e5d833ca3bf069
Dean Luick <dean.luick@cornelisnetworks.com>
2025-12-13 12:21:04 -0500

    RDMA/hfi1: Separate individual SDMA interupts
    Each SDMA engine has 3 possible interrupt sources.  Separate them
    into their own MSI-X interrupt.
    
    INTERNAL: STL-75587
    INTERNAL: STL-75983
    
    Signed-off-by: Dean Luick <dean.luick@cornelisnetworks.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/sdma.h
+++ drivers/infiniband/hw/hfi1/sdma.h
@@ -444,7 +444,7 @@ struct sdma_engine {
 	struct list_head flushlist;
 	struct cpumask cpu_mask;
 	struct kobject kobj;
-	u32 msix_intr;
+	u32 msix_intr[3];
 };
 
 int sdma_init(struct hfi1_devdata *dd);
